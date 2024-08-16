d389410019b8f895a768afbf37f9262ae7f09cf3
DontNeg <lukianmokahel123@gmail.com>
2024-08-15 20:28:05 -0700

    minor changes 1.21.1

--- src/main/java/dontneg/noteblockcharged/NoteBlockChargedClient.java
+++ src/main/java/dontneg/noteblockcharged/NoteBlockChargedClient.java
@@ -2,8 +2,8 @@ package dontneg.noteblockcharged;
 
 import net.fabricmc.api.ClientModInitializer;
 
+@SuppressWarnings("unused")
 public class NoteBlockChargedClient implements ClientModInitializer {
-
     @Override
     public void onInitializeClient() {
         NoteBlockCharged.LOGGER.info("Note Block Charged - Client Initializing!");    }
